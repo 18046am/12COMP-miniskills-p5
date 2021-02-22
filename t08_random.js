88c1f8f8dfbd5867dd5a96442993e713bbb50206
Alan McIlwaine <18046am@hvhs.school.nz>
2021-02-22 00:30:26 +0000

    t08 109 finished

--- t08_random.js
+++ t08_random.js
@@ -1,33 +1,48 @@
 var ball = {
-	x:150,
-	y:200,
+	x:100,
+	y:100,
 	w:50,
 	h:50,
 	moveX: 5,
-	moveY: 0,
+	moveY: 5,
 	radius: function(){
 		return (this.w / 2);
 	}
 }
 
 function setup(){
-	createCanvas(400,400)
+	createCanvas(400,400);
+	ball.x = random(100,300);
+	ball.y = random(100,300)
 }
+
+
 function draw(){
 	background(125, 197, 219);
 	fill(125, 197, 219);
-	strokeWeight(3);
 	ellipse(ball.x, ball.y, ball.w, ball.h);
 	ball.x = ball.x + ball.moveX;
 	ball.y = ball.y + ball.moveY;
+
+
 	if (ball.x > width - ball.radius()) {
 		ball.x = width - ball.radius();
-		ball.moveX = ball.moveX * -1;
+		ball.moveX = ball.moveX * -1 + random(-0.1, 0.1);
+		console.log("X Velocity: " + ball.moveX);
 	}else if (ball.x < ball.radius()){
 		ball.x = ball.radius();
 		ball.moveX = ball.moveX * -1;
-		
+		console.log("X Velocity: " + ball.moveX);
 	}
 
-
+	if (ball.y > height - ball.radius()) {
+		ball.y = height - ball.radius();
+		ball.moveY = ball.moveY * -1 + random(-0.1, 0.1);
+		console.log("Y Velocity: " + ball.moveY);
+	}else if (ball.y < ball.radius()){
+		ball.y = ball.radius();
+		ball.moveY = ball.moveY * -1;
+		console.log("Y Velocity: " + ball.moveY);
+	}
 }
+   
--- t09_functions.js
+++ t09_functions.js
@@ -1,10 +1,59 @@
-/********************************************************/
-//
-/********************************************************/
-function setup() {
-  createCanvas(400, 400);
+var ball = {
+	x:100,
+	y:100,
+	//width
+	w:50,
+	//height
+	h:50,
+	//x velocity
+	moveX: 5,
+	//y velocity
+	moveY: 5,
+	radius: function(){
+		return (this.w / 2);
+	},
+	
+	//ball physics against x wall
+	bounceX: function (){
+		if (ball.x > width - ball.radius()) {
+			ball.x = width - ball.radius();
+			ball.moveX = ball.moveX * -1 + random(-0.1, 0.1);
+			console.log("X Velocity: " + ball.moveX);
+		}else if (ball.x < ball.radius()){
+			ball.x = ball.radius();
+			ball.moveX = ball.moveX * -1;
+			console.log("X Velocity: " + ball.moveX);
+		}
+	},
+	//ball physics against y wall
+	bounceY: function (){
+		if (ball.y > height - ball.radius()) {
+			ball.y = height - ball.radius();
+			ball.moveY = ball.moveY * -1 + random(-0.1, 0.1);
+			console.log("Y Velocity: " + ball.moveY);
+		}else if (ball.y < ball.radius()){
+			ball.y = ball.radius();
+			ball.moveY = ball.moveY * -1;
+			console.log("Y Velocity: " + ball.moveY);
+		}
+	},
 }
 
-function draw() {
-  background(220);
+
+function setup(){
+	createCanvas(400,400);
+	ball.x = random(100,300);
+	ball.y = random(100,300)
 }
+
+
+function draw(){
+	background(125, 197, 219);
+	fill(125, 197, 219);
+	ellipse(ball.x, ball.y, ball.w, ball.h);
+	ball.x = ball.x + ball.moveX;
+	ball.y = ball.y + ball.moveY;
+	ball.bounceX()
+	ball.bounceY()
+}
+   
